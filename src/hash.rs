674cbd07b44f62e09b750e893debc8726423a51c
Oleksandr Yermakov <olexander.yermakov@gmail.com>
2022-02-05 19:53:12 +0200

    Add Hash trait
    Signed-off-by: Oleksandr Yermakov <olexander.yermakov@gmail.com>

---
+++ src/hash.rs
@@ -0,0 +1,13 @@
+//! Hash.
+
+/// Trait represents Hash.
+pub trait Hash: Zeroize {
+    /// Create new hasher.
+    fn new() -> Self;
+
+    /// Update state using the provided data.
+    fn update(&mut self, data: &[u8]);
+
+    /// Retrieve result and consume hasher instance.
+    fn finalize(self) -> Vec<u8>;
+}
